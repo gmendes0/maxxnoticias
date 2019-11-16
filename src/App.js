6a6896d5c431d84b6dda5ccfb015822fddfe7b42
gmendes0 <gmendes230@gmail.com>
2019-11-16 00:24:06 -0200

    footer adicionado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Routes from './routes'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Routes />
+      <Footer />
     </>
   )
 }
